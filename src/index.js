416d72be78419c3d78c33e27cecc6e7f8b6f0b4d
Samuel Timbó <samuelmtimbo@gmail.com>
2021-05-25 14:31:57 -0300

    serve public for all GET request

--- src/index.js
+++ src/index.js
@@ -15,11 +15,29 @@
 // [START cloudrun_helloworld_service]
 // [START run_helloworld_service]
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const WebSocket = require("ws");
 
-app.use(express.static("public"));
+const CWD = process.cwd();
+
+app.get("*", (req, res) => {
+  const { url } = req;
+  const url_segments = url.split("/");
+  const url_segments_length = url_segments.length;
+  const url_last_segment =
+    url_segments[url_segments_length - 1] || "index.html";
+  let file_name = url_last_segment;
+  if (url_last_segment === "index.js") {
+  } else {
+    file_name = "index.html";
+  }
+  const file_path = path.join(CWD, "public", file_name);
+  res.sendFile(file_path);
+});
+
+// app.use(express.static("public"));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
@@ -28,11 +46,11 @@ const server = app.listen(port, () => {
 
 const wss = new WebSocket.Server({
   server,
-  path: '/'
+  path: "/",
 });
 
 wss.on("connection", function connection(ws, req) {
-  console.error("wss", "connection");
+  console.error("wss", "connection", req.url);
 
   function _send(data) {
     send(ws, data);
