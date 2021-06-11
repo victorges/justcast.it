3f76baff5a5ced092c492c414b3bab1e13eeca42
Victor Elias <victorgelias@gmail.com>
2021-06-11 10:32:25 -0300

    create helper on trasmitter to connect WebRTC

--- client/src/transmitter/index.ts
+++ client/src/transmitter/index.ts
@@ -57,6 +57,69 @@ let _streamKey: string | undefined
 
 let _mimeType: string | undefined
 
+async function iceHandshake(
+  streamKey: string,
+  localDesc: RTCSessionDescription
+) {
+  const answer = await fetch(
+    `https://rtmp.justcast.it/webrtc/offer?streamKey=${streamKey}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(localDesc),
+      headers: {
+        ['content-type']: 'application/json',
+      },
+    }
+  )
+  if (answer.status !== 200) {
+    throw new Error(`Error response from server: ${answer.status}`)
+  }
+  return (await answer.json()) as RTCSessionDescriptionInit
+}
+
+async function connetWebRTC(
+  stream: MediaStream,
+  streamKey: string,
+  onConnected?: () => void,
+  onClosed?: () => void
+) {
+  const pc = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: 'stun:stun.l.google.com:19302',
+      },
+    ],
+  })
+
+  stream.getTracks().forEach((track) => pc.addTrack(track, stream))
+
+  pc.oniceconnectionstatechange = () =>
+    console.log('webrtc ice connection state', pc.iceConnectionState)
+  pc.onicegatheringstatechange = () => {
+    if (pc.iceGatheringState !== 'complete') {
+      return
+    }
+    iceHandshake(streamKey, pc.localDescription)
+      .then(async (remoteDesc) => {
+        await pc.setRemoteDescription(new RTCSessionDescription(remoteDesc))
+      })
+      .catch(console.log)
+  }
+  pc.onconnectionstatechange = () => {
+    const state = pc.connectionState
+    console.log('webrtc connection state', state)
+    switch (state) {
+      case 'connected':
+        if (onConnected) onConnected()
+      case 'closed':
+        if (onClosed) onClosed()
+    }
+  }
+
+  const offer = await pc.createOffer()
+  await pc.setLocalDescription(offer)
+}
+
 function initMimeType() {
   if (!window.MediaRecorder) {
     return
