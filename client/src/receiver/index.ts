f4d2c3c06656e65d4039d6062154d69e238eb138
Victor Elias <victorgelias@gmail.com>
2021-07-07 15:53:49 -0300

    Support playback from playbackUrl

--- client/src/receiver/index.ts
+++ client/src/receiver/index.ts
@@ -22,13 +22,16 @@ player.controls(true)
 
 const humanId = pathname.substr(1)
 fetch(`/api/stream/${humanId}`)
-  .then((res) => {
-    if (res.status === 200) {
-      return res.json()
-    }
-    const playbackUrl = `https://cdn.livepeer.com/hls/${humanId}/index.m3u8`
-    return { playbackUrl }
-  })
+  .then(
+    (res) => {
+      if (res.status === 200) {
+        return res.json()
+      }
+      const playbackUrl = `https://cdn.livepeer.com/hls/${humanId}/index.m3u8`
+      return { playbackUrl }
+    },
+    (err) => ({ playbackUrl: humanId }) // try path as playback URL itself
+  )
   .then((info) => {
     player.src({
       src: info.playbackUrl,
