8305ab80f336e3390a92b6fb8441d49e701a643b
Victor Elias <victorgelias@gmail.com>
2021-07-26 19:38:16 -0300

    Fix nginx lb update script

--- nginx/snippets/update_endpoints.sh
+++ nginx/snippets/update_endpoints.sh
@@ -4,6 +4,10 @@ set -e
 ig_name=go-webrtmp-ig-ue1
 output_file=/etc/nginx/snippets/webrtmp-group-ips.conf
 
+function log() {
+	echo "[$(date -Isecond)]" $@
+}
+
 function get_endpoints() {
 	set -e
 
@@ -16,24 +20,25 @@ function get_endpoints() {
 	done
 }
 
-echo Fetching endpoints from instance group $ig_name...
+log Fetching endpoints from instance group $ig_name...
 endpoints=$(get_endpoints $1)
 
 if echo "$endpoints" | cmp -s - "$output_file"; then
-	echo "Endpoints already updated"
+	log "Endpoints already updated"
 else
-	echo "Writing endpoints to file $output_file"
-	echo "Endpoints:"
-	echo "$endpoints"
+	log "Writing endpoints to file $output_file"
+	log "Endpoints:"
+	log "$endpoints"
 
 	echo "$endpoints" > "$output_file"
 fi
 
-echo Testing nginx config...
-if service nginx configtest; then
-	echo Reloading...
-	service nginx reload
-	echo Success!
+log Testing nginx config...
+if /usr/sbin/service nginx configtest; then
+	log Reloading...
+	/usr/sbin/service nginx reload
+	log Success!
 else
-	nginx -t
+	log Failed!
+	/usr/sbin/nginx -t
 fi
