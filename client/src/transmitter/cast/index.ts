00e26c33ff45a821c2a41110b8a0dae9333d0fe6
Samuel Timbó <samuelmtimbo@gmail.com>
2021-09-17 17:10:45 -0300

    use webrtmp-sdk

--- client/src/transmitter/cast/index.ts
+++
@@ -1,8 +0,0 @@
-import castViaWebSocket, { mimeType } from './websocket'
-import castViaWebRTC from './webrtc'
-
-export default {
-  wsMimeType: mimeType,
-  viaWebSocket: castViaWebSocket,
-  viaWebRTC: castViaWebRTC,
-}
