19b731af61018a992f87859b9fd26f1453b81134
Samuel Timbó <samuelmtimbo@gmail.com>
2023-04-30 19:35:00 -0300

    allow cors (#44)

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,21 @@ require('dotenv').config()
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
 app.use(cookieParser())
 
 // must be after the call to expressWs above
 import * as handlers from './handlers'
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 app.use('/api', handlers.api)
 app.use('/', handlers.files)
